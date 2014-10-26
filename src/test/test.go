6c9f4a74f0709805cf124de33db2dff36331e52b
wneo <wuyuan2012@gmail.com>
2014-10-26 22:03:38 +0800

    golangTest

---
+++ src/test/test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		fmt.Println("i=", i, s)
+		runtime.Gosched()
+		fmt.Println(s)
+
+	}
+}
+
+func main() {
+	fmt.Println("Start ...")
+
+	go say("world") //开一个新的Goroutines执行
+	say("hello")    //当前Goroutines执行
+	// ch := make(chan int, 1)
+	// countYes := 0
+	// countNo := 0
+	// for {
+	// 	select {
+	// 	case ch <- 0:
+	// 	case ch <- 1:
+	// 	}
+
+	// 	i := <-ch
+	// 	if i == 1 {
+	// 		countYes++
+	// 	} else {
+	// 		countNo++
+	// 	}
+
+	// 	fmt.Println(countYes, countNo, "Value received:", i)
+	// }
+}
